75b927d16ca1767f04c08dbdedcf573a68db2bff
stanislav.bieliaiev <stanislav.bieliaiev@KBP1-LDL-A15327.synapse.com>
2022-10-05 13:28:21 +0300

    fix(APPINT-34581) - fixed job feature version relation to cloud

--- main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
+++ main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
@@ -231,12 +231,20 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
 
     private FeaturesModel getFeatureModel(File tmpJob) {
         ProcessItem processItem = (ProcessItem) node.getObject().getProperty().getItem();
-        FeaturesModel featuresModel = new FeaturesModel(groupId, artifactName, artifactVersion);
+        FeaturesModel featuresModel = new FeaturesModel(groupId, artifactName, getFeaturetVersion(processItem.getProperty()));
         featuresModel.setConfigName(node.getObject().getLabel());
         featuresModel.setContexts(JobContextUtils.getContextsMap(processItem));
-        BundleModel bundleModel = new BundleModel(groupId, artifactName, artifactVersion, tmpJob);
+        BundleModel bundleModel = new BundleModel(groupId, artifactName, getBundleVersion(processItem.getProperty()), tmpJob);
         featuresModel.addBundle(bundleModel);
         return featuresModel;
+    }
+
+    protected String getFeaturetVersion(Property property) {
+        return PomIdsHelper.getFeatureVersion(property, artifactVersion);
+    }
+
+    protected String getBundleVersion(Property property) {
+        return PomIdsHelper.getBundleVersion(property, artifactVersion);
     }
 
     protected String getArtifactVersion() {
